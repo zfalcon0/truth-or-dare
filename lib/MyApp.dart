b95dafe3e80a35b159204f0b3843676030d6ab70
zfalcon0 <biruktelavive7@gmail.com>
2021-04-03 12:00:53 +0100

    create new class and add gif files on onboarding screen

---
+++ lib/MyApp.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_onboard/flutter_onboard.dart';
+import 'package:onb/MyApp.dart';
+import 'package:provider/provider.dart';
+import 'dart:math';
+
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<HomePage> with TickerProviderStateMixin {
+  var lastposition = 0.0;
+  var random = new Random();
+  AnimationController animationController;
+
+  get duration => null;
+
+  double getRandomNumber() {
+    lastposition = random.nextDouble();
+    return lastposition;
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 5)
+        );
+  }
+
+  spinTheBottle() {
+    animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 5)
+        );
+    animationController.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var curves;
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: MediaQuery
+                .of(context)
+                .size
+                .height,
+            child: Image.asset("assets/td.jpg",
+                fit: BoxFit.fill),
+
+          ),
+          Center(
+            child: Container(
+              child: RotationTransition(
+                turns: Tween(begin: lastposition, end: getRandomNumber())
+                    .animate(
+                    new CurvedAnimation(
+                        parent: animationController, curve: Curves.linear)),
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      spinTheBottle();
+                    });
+                  },
+                  child: Image.asset("assets/bottle.jpg",
+                    width: 250, height: 250,
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_onboard/flutter_onboard.dart';
+import 'package:onb/MyApp.dart';
 import 'package:provider/provider.dart';
 import 'dart:math';
 
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 void main() {
-  runApp(App());
+  runApp(App(new MyApp()));
 }
 
 class App extends StatelessWidget {
+  App(MyApp myApp);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: AnimatedSplashScreen(
-        splash: Image.asset('assets/tdicon.jpg'),
+        splash: Image.asset(
+          'assets/tdgif.gif',
+          height: 10000,
+          width: 10000,),
         nextScreen: HomeScreen(),
         splashTransition: SplashTransition.slideTransition,
         backgroundColor: Colors.amber,
         duration: 3000,
+
       ),
     );
   }
@@ -113,102 +120,19 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomePage(),
-    );
-  }
-}
 
-
-class HomePage extends StatefulWidget {
-  @override
-  _HomepageState createState() => _HomepageState();
-}
-
-class _HomepageState extends State<HomePage> with TickerProviderStateMixin {
-  var lastposition = 0.0;
-  var random = new Random();
-  AnimationController animationController;
-
-  get duration => null;
-
-  double getRandomNumber() {
-    lastposition = random.nextDouble();
-    return lastposition;
-  }
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    animationController =
-        AnimationController(vsync: this, duration: Duration(seconds: 5)
-        );
-  }
-
-  spinTheBottle() {
-    animationController =
-        AnimationController(vsync: this, duration: Duration(seconds: 5)
-        );
-    animationController.forward();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    var curves;
-    return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          Container(
-            height: MediaQuery
-                .of(context)
-                .size
-                .height,
-            child: Image.asset("assets/td.jpg",
-                fit: BoxFit.fill),
-
-          ),
-          Center(
-            child: Container(
-              child: RotationTransition(
-                turns: Tween(begin: lastposition, end: getRandomNumber())
-                    .animate(
-                    new CurvedAnimation(
-                        parent: animationController, curve: Curves.linear)),
-                child: GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      spinTheBottle();
-                    });
-                  },
-                  child: Image.asset("assets/bottle.jpg",
-                    width: 250, height: 250,
-                  ),
-                ),
-              ),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
 
 final List<OnBoardModel> onBoardData = [
   const OnBoardModel(
-    title: "Set your own goals and get better",
-    description: "Goal support your motivation and inspire you to work harder",
-    imgUrl: "assets/bottle.jpg",
+    title: "Play Truth Or Dare",
+    description: "Get to know your friends by playing a fun game of Truth Or Dare! ",
+    imgUrl: "assets/tdgif2.gif",
   ),
   const OnBoardModel(
     title: "Track your progress with statistics",
     description:
     "Analyse personal result with detailed chart and numerical values",
-    imgUrl: 'assets/td,jpg',
+    imgUrl: 'assets/tdgif.gif',
   ),
   const OnBoardModel(
     title: "Create photo comparissions and share your results",
